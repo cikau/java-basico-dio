210eab2e666cd54991ade184e62500299d3a0163
Cikau <cika.angel@gmail.com>
2024-08-09 16:12:46 -0300

    Update Main e SmartTV

--- src/aulas/metodos/sistemaSmartTV/Main.java
+++ src/aulas/metodos/sistemaSmartTV/Main.java
@@ -4,8 +4,28 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        SmartTv smartTv = new SmartTv;
-        
+        SmartTv smartTv = new SmartTv(); //criar ou instanciar novo objeto
+
+        System.out.println("A TV está ligada? " + smartTv.ligada); //chama atributo e os seus valores padrão
+        System.out.println("A TV está em que canal? " +smartTv.canal);
+        System.out.println("A TV está em que volume? " +smartTv.volume);
+
+        smartTv.ligar(); //serve para acionar o método de ligar a TV
+        System.out.println("A TV está ligada? " + smartTv.ligada); //atualiza o status da TV
+
+        smartTv.desligar(); //serve para acionar o método de desligar a TV
+        System.out.println("A TV está ligada? " + smartTv.ligada); //atualiza o status da TV
+
+        //volume inicial: 10
+    smartTv.diminuirVolume(); // 10 - 1 = 9
+    smartTv.diminuirVolume(); // 9 - 1 = 8
+    smartTv.diminuirVolume(); // 8 - 1 = 7
+    smartTv.aumentarVolume(); // 7 + 1 = 8
+    System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+    smartTv.mudarCanal(13); //inserção de novo valor, entre parênteses, para o canal
+        System.out.println("Novo canal: " + smartTv.canal);
 
     }
 }
--- src/aulas/metodos/sistemaSmartTV/SmartTv.java
+++ src/aulas/metodos/sistemaSmartTV/SmartTv.java
@@ -1,4 +1,41 @@
 package aulas.metodos.sistemaSmartTV;
 
 public class SmartTv {
+
+    //atributos da classe SmartTV
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 10;
+
+    //métodos da classe SmartTV
+    public void ligar() {  //método que muda o valor do boolean ligada para "true"
+        ligada = true;
+    }
+
+    public void desligar() {  //método que muda o valor do boolean ligada para "false"
+        ligada = false;
+    }
+
+public void aumentarVolume() {
+        volume++;
+    System.out.println("Aumentando o volume para: " + volume);
+}
+
+public void diminuirVolume() {
+        volume--;
+    System.out.println("Reduzindo o volume para: " + volume);
+}
+
+public void aumentarCanal() {
+        canal++;
+}
+
+public void diminuirCanal() {
+        canal--;
+}
+
+public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+}
+
 }
