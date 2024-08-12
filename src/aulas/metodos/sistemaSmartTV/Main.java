7c1c26712606a929f39740f6def9526b8869528a
Cikau <cika.angel@gmail.com>
2024-08-12 12:43:02 -0300

    Mudanças Main

--- src/aulas/metodos/sistemaSmartTV/Main.java
+++ src/aulas/metodos/sistemaSmartTV/Main.java
@@ -4,28 +4,28 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        SmartTv smartTv = new SmartTv(); //criar ou instanciar novo objeto
+        SmartTv televisaoLG = new SmartTv(); //criar ou instanciar novo objeto
 
-        System.out.println("A TV está ligada? " + smartTv.ligada); //chama atributo e os seus valores padrão
-        System.out.println("A TV está em que canal? " +smartTv.canal);
-        System.out.println("A TV está em que volume? " +smartTv.volume);
+        System.out.println("A TV está ligada? " + televisaoLG.ligada); //chama atributo e os seus valores padrão
+        System.out.println("A TV está em que canal? " + televisaoLG.canal);
+        System.out.println("A TV está em que volume? " + televisaoLG.volume);
 
-        smartTv.ligar(); //serve para acionar o método de ligar a TV
-        System.out.println("A TV está ligada? " + smartTv.ligada); //atualiza o status da TV
+        televisaoLG.ligar(); //serve para acionar o método de ligar a TV
+        System.out.println("A TV está ligada? " + televisaoLG.ligada); //atualiza o status da TV
 
-        smartTv.desligar(); //serve para acionar o método de desligar a TV
-        System.out.println("A TV está ligada? " + smartTv.ligada); //atualiza o status da TV
+        televisaoLG.desligar(); //serve para acionar o método de desligar a TV
+        System.out.println("A TV está ligada? " + televisaoLG.ligada); //atualiza o status da TV
 
         //volume inicial: 10
-    smartTv.diminuirVolume(); // 10 - 1 = 9
-    smartTv.diminuirVolume(); // 9 - 1 = 8
-    smartTv.diminuirVolume(); // 8 - 1 = 7
-    smartTv.aumentarVolume(); // 7 + 1 = 8
-    System.out.println("Volume atual: " + smartTv.volume);
+        televisaoLG.diminuirVolume(); // 10 - 1 = 9
+        televisaoLG.diminuirVolume(); // 9 - 1 = 8
+        televisaoLG.diminuirVolume(); // 8 - 1 = 7
+        televisaoLG.aumentarVolume(); // 7 + 1 = 8
+            System.out.println("Volume atual: " + televisaoLG.volume);
 
-        System.out.println("Canal atual: " + smartTv.canal);
-    smartTv.mudarCanal(13); //inserção de novo valor, entre parênteses, para o canal
-        System.out.println("Novo canal: " + smartTv.canal);
+        System.out.println("Canal atual: " + televisaoLG.canal);
+        televisaoLG.mudarCanal(13); //inserção de novo valor, entre parênteses, para o canal
+        System.out.println("Novo canal: " + televisaoLG.canal);
 
     }
 }
