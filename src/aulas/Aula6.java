16a512ab1c199d8fa8a131eaa6bc4fa2bfab1e02
Cikau <cika.angel@gmail.com>
2024-08-13 14:32:04 -0300

    MAIN ARGS

--- src/aulas/Aula6.java
+++ src/aulas/Aula6.java
@@ -5,7 +5,18 @@ public class Aula6 {
     /*
     TERMINAL E ARGUMENTOS EM JAVA
 
-MAIN ARGS:
+ MAIN ARGS: quando executamos uma classe que contenha o método MAIN,
+ ele permite que passemos um Array [ ] de argumentos String. Logo, é possível
+ esses parâmetros após definir a classe a ser executada.
+ EX:
+ public class AboutMe {
+ public static void main(String[ ] args) {
+ String nome = args [0]; --> o número inicial do índice começa SEMPRE em ZERO
+ String sobrenome = args [1];
+ int idade = Integer.valueOf(args[2]);
+ double altura = Double.valueOf(args[3]);
+    }
+ }
 
 SCANNER:
 
---
+++ src/aulas/metodos/sistemaSmartTV/AboutMe.java
@@ -0,0 +1,23 @@
+package aulas.metodos.sistemaSmartTV;
+//testando os argumentos no Main
+public class AboutMe {
+    public static void main(String[] args) {
+
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Olá, eu me chamo " + nome + " " + sobrenome + " .");
+        System.out.println("Tenho " + idade + " anos e " + altura + " de altura.");
+
+    }
+
+}
+
+/*
+Quando executarmos o código acima no prompt de comando do Windows, poderemos
+inserir as informações - seguindo a ordem do código - para aparecer lá. Primeiro,
+nome, depois sobrenome, idade e, por último, a altura.
+
+*/
